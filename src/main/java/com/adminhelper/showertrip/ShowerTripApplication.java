da42fa58e1aa2311e54d9d84dc2fbab0f090e14f
adminhelper <201401339hs@gmail.com>
2023-04-14 10:12:16 +0900

    init : new project

---
+++ src/main/java/com/adminhelper/showertrip/ShowerTripApplication.java
@@ -0,0 +1,13 @@
+package com.adminhelper.showertrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowerTripApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShowerTripApplication.class, args);
+    }
+
+}
